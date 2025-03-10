3c731821104b63a607f8b7f4f282a640f929fa12
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2025-03-10 07:18:07 -1200

    operador not

---
+++ operadores_logicos.py
@@ -0,0 +1,12 @@
+'''
+ 
+ operadores logicos 
+ 
+ Aquellos que operan unica mente 
+ con valores booleanos (V F)
+ Acorde a las tablas de verad 
+ '''
+ 
+ #Ejemplo 1: operadores not:
+y=not True 
+print("el resultado de operar con not es " ,y)
