e292540a05e182a4cb505a596cab9f886f1fb738
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2025-03-10 09:59:11 -1200

    operador or

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -14,3 +14,7 @@ print("el resultado de operar con not es " ,y)
 #Ejemplo 2: operador and:
 y = False and True 
 print("el resultado de operar con and es " ,y)
+
+#ejemplo 3: operador or 
+y= False or False 
+print ("el resultado de operar con and es" ,y)
